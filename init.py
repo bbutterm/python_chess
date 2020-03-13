0c2d8d0fad4962a3ff5aee27a35e138d438d458e
bbutterm <54075794+bbutterm@users.noreply.github.com>
2020-03-13 17:40:41 +0300

    Update init.py

--- init.py
+++ init.py
@@ -1,4 +1,19 @@
 #len 1-8, heigh a-h
+def change_value(value,team):
+  s = ""
+  if team == 0:
+    s +=value
+  else:
+    s += '\033[96m'
+    s += value
+    s += '\033[0m'
+  return (s)
+
+def print_line(list):
+  s = ""
+  for i in list:
+    s+=i
+  print(s)
 class Board():
   board = []
   height = 0
@@ -15,14 +30,33 @@ class Board():
     for i in self.height_curve:
       self.board[j].append(i)
       j+=1
-def print_line(list):
-  s = ""
-  for i in list:
-    s+=i
-  print(s)
+  def print_board(self):
+    for i in self.board:
+      print_line(i)
+    print_line(a.width_curve)
+class Figure():
+  x = 0
+  y = 0
+  rank = 0
+  team = 0
+  value = " k "
+  def __init__(self,b = Board.board,x = 0,y = 0,team = 0):
+    self.team = team
+    self.x = x
+    self.y = y
+    b[x][y] = change_value(self.value,self.team)
+  def info(self):
+    print(self.x," ",self.y)
+class Pawn(Figure):
+  rank = 1
+  team = 0
+  value = " p "
+  
+
 a = Board()
-j = 0
-for i in a.board:
-  print_line(i)
-  j+=1
-print_line(a.width_curve)
+b = a.board
+a.print_board()
+p = Pawn(b,x = 1,y = 1,team = 1)
+a.print_board()
+f = change_value("jopa",1)
+print(f)
